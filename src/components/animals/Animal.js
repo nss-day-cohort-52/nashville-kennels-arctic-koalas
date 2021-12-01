70284f738894c03fbf6b7418693fec4ef9e1cb2c
Sean Derevjanik <sean.derevjanik@gmail.com>
2021-12-01 13:08:52 -0700

    correct ternary logic for map function

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -100,12 +100,10 @@ export const Animal = ({ animal, syncAnimals,
                                             </option>
                                             {
                                                 employees.map(availableCaretaker => { //for each possible caretaker
-                                                    (currentAnimal.animalCaretakers.find(currentCaretaker => currentCaretaker.user.id === availableCaretaker.id)) ? //checks if they are already caring for an animal  
+                                                    return (currentAnimal.animalCaretakers.find(currentCaretaker => currentCaretaker.user.id === availableCaretaker.id)) ? //checks if they are already caring for an animal. note you need the return before the ternary starts  
                                                     null : //if they are, leave them off the select
-                                                    (<option key={availableCaretaker.id} value={availableCaretaker.id}>{availableCaretaker.name}</option>) //otherwise, add them as an option
-                                                }     
-                                                )
-                                            }
+                                                    <option key={availableCaretaker.id} value={availableCaretaker.id}>{availableCaretaker.name}</option> //otherwise, add them as an option
+                                            })}
 
                                         </select>
                                         <button className="btn btn-warning mt-3 form-control small" onClick={() =>
