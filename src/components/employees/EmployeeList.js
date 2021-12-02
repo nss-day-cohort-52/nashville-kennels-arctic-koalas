9f80b848bb9dc7752d1b40a41f259f422ddcea6d
Sean Derevjanik <sean.derevjanik@gmail.com>
2021-12-02 09:10:37 -0700

    added ability to fire employees, only shows for employee users

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -6,10 +6,13 @@ import "./EmployeeList.css"
 
 export default () => {
     const [emps, setEmployees] = useState([])
+    const syncEmployees = () => { //created function to allow calls from empolyee while on the list
+        EmployeeRepository.getAll().then(data=>setEmployees(data))
+    }
 
     useEffect(
         () => {
-            EmployeeRepository.getAll().then(setEmployees)
+            syncEmployees() //replaced with the function call created above
         }, []
     )
 
@@ -17,7 +20,7 @@ export default () => {
         <>
             <div className="employees">
                 {
-                    emps.map(a => <Employee key={a.id} employee={a} />)
+                    emps.map(a => <Employee key={a.id} employee={a} syncEmployees={syncEmployees} />) //passed sync employees to allow employee component to force employee list to rerender
                 }
             </div>
         </>
--- src/repositories/EmployeeRepository.js
+++ src/repositories/EmployeeRepository.js
@@ -12,7 +12,14 @@ export default {
                 return userWithRelationships
             })
     },
+
+    async wait(ttw) { return new Promise((resolve) => setTimeout(resolve,ttw))
+
+    },
+
     async delete(id) {
+        // console.log("Fired an employee")
+        // return await this.wait(2000)
         return await fetchIt(`${Settings.remoteURL}/users/${id}`, "DELETE")
     },
     async addEmployee(employee) {
